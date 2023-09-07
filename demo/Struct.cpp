7eafe98fa49223abfaf5b93976f343c40f2bf831
张海锋 <zhang1287609286@qq.com>
2023-09-07 17:57:22 +0800

    修改

--- demo/Struct.cpp
+++ demo/Struct.cpp
@@ -7,6 +7,21 @@ struct person
     string name;
 };
 
+struct message
+{
+    unsigned int status : 4;
+    unsigned int : 4; // 预留
+    int value : 4;
+};
+
+union one4all
+{
+    int int_val;
+    long long_val;
+    double double_val;
+    char char_val;
+};
+
 int main()
 {
     person p1 = {
@@ -14,4 +29,22 @@ int main()
         "John",
     };
     cout << p1.age << " " << p1.name << endl;
+
+    struct student
+    {
+        string name;
+        string hobbiy;
+    } student = {
+        "Zhang",
+        "Computer"};
+    cout << student.name << " " << student.hobbiy << endl;
+
+    message m1 = {1, 6};
+    cout << m1.status << " " << m1.value << endl;
+
+    one4all val = {65};
+    cout << val.int_val << endl;
+    cout << val.long_val << endl;
+    cout << val.double_val << endl;
+    cout << val.char_val << endl;
 }
